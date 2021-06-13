22ce37f026136fc219263882a46c0fb7084ca4f6
kevindoubleu <kevin.wibamanto@gmail.com>
2021-06-13 10:24:35 +0700

    fix linked lists index methods

--- data-structures/linked-list/doubly/doubly.go
+++ data-structures/linked-list/doubly/doubly.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 type Node struct {
+	prev *Node
 	data int
 	next *Node
-	prev *Node
 }
 
 type LinkedList struct {
@@ -75,6 +75,95 @@ func (ll *LinkedList) clear() {
 	// in golang, circular references can be garbage collected
 	ll.head = nil
 	ll.tail = nil
+}
+
+func (ll *LinkedList) get(index int) *Node {
+	current := ll.head
+	for i := 0; i < index; i++ {
+		// if out of bounds
+		if current == nil {
+			return nil
+		}
+		current = current.next
+	}
+	return current
+}
+
+func (ll *LinkedList) remove(index int) *Node {
+	current := ll.head
+	for i := 0; i < index-1; i++ {
+		if current == nil {
+			return nil
+		}
+		current = current.next
+	}
+
+	// edge cases
+	if index == 0 {
+		return ll.popFirst()
+	} else if current.next.next == nil {
+		return ll.popLast()
+	// the node we want to remove is current.next
+	// because the loop is only until index-1
+	} else {
+		tmp := current.next
+
+		current.next.next.prev = current
+		current.next = current.next.next
+
+		return tmp
+	}
+}
+
+func (ll *LinkedList) insert(index int, n Node) {
+	// edge cases
+	if index == 0 {
+		ll.prepend(n)
+	} else if index >= ll.size() {
+		ll.append(n)
+	// inserting in the middle of list
+	} else {
+		current := ll.head
+		for i := 0; i < index-1; i++ {
+			current = current.next
+		}
+		// we want to put new node in current.next
+		// because the loop is only until index-1
+		n.next = current.next
+		n.prev = current
+		current.next.prev = &n
+		current.next = &n
+	}
+}
+
+func (ll LinkedList) print() {
+	var current *Node
+	for current = ll.head; current != nil; current = current.next {
+		fmt.Printf("%p <- %p(%v) -> %p\n", current.prev, current, current.data, current.next)
+	}
+}
+
+func (ll LinkedList) size() int {
+	len := 0
+	for current := ll.head; current != nil; current = current.next {
+		len++
+	}
+	return len
+}
+
+// for use as a loop iterator
+func (ll *LinkedList) getIterator() func() *Node {
+	current := ll.head
+	return func() *Node {
+		if current != nil {
+			defer func() {
+				current = current.next
+			}()
+			return current
+		} else {
+			return nil
+		}
+	}
 }
 
 func main() {
