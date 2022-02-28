d24725e1491e6269a4163cc013298f4191072a2e
kevindoubleu <kevin.wibamanto@gmail.com>
2022-02-28 12:36:10 +0700

    fix binary search off by one error

--- algorithms/searching/binary/main.go
+++ algorithms/searching/binary/main.go
@@ -10,7 +10,7 @@ func main() {
 	sort.Ints(arr) // binary search only work on sorted arrays
 	fmt.Println(arr)
 
-	findThese := []int{1, 2, 3, 65}
+	findThese := []int{1, 2, 3, 65, 23, 43, 65, 7, 21, 3, 652, 2}
 	for _, target := range findThese {
 		fmt.Println("searching for", target, "got", search(arr, target))
 	}
@@ -20,17 +20,17 @@ func search(arr []int, target int) int {
 	left, right := 0, len(arr)-1
 	mid := 0
 
-	for left < right {
+	for left <= right {
 		mid = (right + left) / 2
 
 		if arr[mid] == target {
 			return mid
 		} else if arr[mid] < target {
 			// target is on the right
-			left = mid
+			left = mid+1
 		} else if arr[mid] > target {
 			// target is on the left
-			right = mid
+			right = mid-1
 		}
 	}
 	return -1
